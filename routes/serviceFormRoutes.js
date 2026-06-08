21db7fba7130b49d204eb205658693a7acb3532d
Onur Gonullu <onr.gnll@outlook.com>
2026-06-08 10:49:29 +0300

    fix

--- routes/serviceFormRoutes.js
+++ routes/serviceFormRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const serviceFormController = require("../controllers/ServiceFormController");
+const serviceFormController = require("../controllers/serviceFormController");
 const { protect, authorize } = require("../middlewares/authMiddleware");
 const { paginationQuery } = require("../validators/commonValidator");
 const upload = require("../middlewares/upload");
