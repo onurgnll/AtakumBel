5b44c8b4da76b0abafceaa4d7229f12059729ca2
Gokay Kokdemir <ygkdemr@hotmail.com>
2026-09-16 09:41:14 +0300

    Update projectController.js

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -22,7 +22,6 @@ exports.getAllProjects = async (req, res, next) => {
       where: whereCondition,
       limit,
       offset,
-      attributes: { exclude: ["content"] },
       order: [["name", "ASC"]],
       distinct: true,
     });
